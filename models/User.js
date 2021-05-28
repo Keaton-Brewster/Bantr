71297023c618eee4c5a91bf5ba7fda0f654d1d09
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-28 09:32:08 -0500

    just getting things set up to begin building user interface

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  name: String,
+  phoneNumber: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const http = require("http");
+const socket = require("socket.io");
+const session = require("express-session");
+const path = require("path");
+const passport = require("passport");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// const router = require("../controllers");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(router);
+
+// Check for production environment before trying to set up the constant service of the index.html
+if (process.env.NODE_ENV === "production") {
+  // If no API routes are hit, send the React app
+  const root = path.join(__dirname, "../client", "build");
+  app.use(express.static(root));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(root, "index.html"));
+  });
+}
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/devlr", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+// Set up for initializing socket as a part of our server
+const server = http.createServer(app);
+const io = socket(server);
+require("./config/socket")(io);
+
+try {
+  server.listen(PORT, () => {
+    console.log("Server online");
+  });
+} catch (error) {
+  throw new Error(
+    `Error initializing server --- /server/index.js --- ERROR: ${error}`
+  );
+}
