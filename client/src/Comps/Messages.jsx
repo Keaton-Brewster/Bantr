300100ca2d61767d0471c27c3faeb1094328686b
Keaton-Brewster <keaton.brewster@gmail.com>
2021-06-09 11:52:17 -0500

    tweaks

--- client/src/Comps/Messages.jsx
+++ client/src/Comps/Messages.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import { Navbar, Container, Row, Col } from "react-bootstrap";
+import { Navbar, Container, Row, Col, Spinner } from "react-bootstrap";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { useConversations } from "../utils/ConvorsationProvider";
 import useViewport from "../utils/useViewport";
@@ -31,74 +31,51 @@ export default function Message({ sendMessage }) {
           </button>
         </Navbar>
       ) : null}
-
-      {messages.length === 1 ? (
-        <div>
-          <div
-            className={`my-1 d-flex flex-column ${
-              messages[0].sender_id === user._id
-                ? "align-self-end align-items-end"
-                : "align-items-start"
-            }`}
-          >
-            <div
-              className={`rounded px-2 py-1 ${
-                messages[0].sender_id === user._id
-                  ? "bg-primary text-white"
-                  : "border"
-              }`}
-            >
-              {messages[0].content}
-            </div>
-            <div
-              className={`text-muted small ${
-                messages[0].sender_id === user._id ? "text-right" : ""
-              }`}
-            >
-              {messages[0].sender_id === user._id
-                ? "You"
-                : messages[0].senderName}
-            </div>
-          </div>
-        </div>
-      ) : (
-        messages.map((message, i) => {
-          return (
-            <div key={i}>
-              <div
-                className={`my-1 d-flex flex-column ${
-                  message.sender_id === user._id
-                    ? "align-self-end align-items-end"
-                    : "align-items-start"
-                }`}
-              >
+      <Container id="messages">
+        {messages.length < 0 ? (
+          <Spinner id="spinner" animation="border" />
+        ) : (
+          messages.map((message, i) => {
+            return (
+              <div key={i}>
                 <div
-                  className={`rounded px-2 py-1 ${
+                  className={`my-1 d-flex flex-column ${
                     message.sender_id === user._id
-                      ? "bg-primary text-white"
-                      : "border"
+                      ? "align-self-end align-items-end"
+                      : "align-items-start"
                   }`}
                 >
-                  {message.content}
-                </div>
-                <div
-                  className={`text-muted small ${
-                    message.sender_id === user._id ? "text-right" : ""
-                  }`}
-                >
-                  {message.sender_id === user._id &&
-                  messages[i - 1].sender_id === user._id
-                    ? "You"
-                    : message.senderName}
+                  <div
+                    className={`overflow rounded px-2 py-1 ${
+                      message.sender_id === user._id
+                        ? "bg-primary text-white"
+                        : "border"
+                    }`}
+                  >
+                    {message.content}
+                  </div>
+                  {
+                    // for some reason, this thing breaks when you tryi to select a new conversation? No idea why
+                    /* <div
+                    className={`text-muted small ${
+                      message.sender_id === user._id ? "text-right" : ""
+                    }`}
+                  >
+                    {message.sender_id === user._id &&
+                    messages[i - 1].sender_id === user._id
+                      ? "You"
+                      : message.senderName}
+                  </div> */
+                  }
                 </div>
               </div>
-            </div>
-          );
-        })
-      )}
+            );
+          })
+        )}
+      </Container>
       <Container fluid id="chatBox">
         <Row>
-          <Col xs={10}>
+          <Col xs={7}>
             <textarea ref={textRef} rows="1" id="chatInput" type="text" />
           </Col>
           <Col xs={2}>
