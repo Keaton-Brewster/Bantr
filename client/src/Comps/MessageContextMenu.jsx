474cdfbb21d30323f960d7af1a827d8a76ad8539
Keaton-Brewster <keaton.brewster@gmail.com>
2021-06-23 09:37:36 -0500

    commenting out the attemp to make message context menus work for now. Going to move to other functionality and come back to this later

--- client/src/Comps/MessageContextMenu.jsx
+++ client/src/Comps/MessageContextMenu.jsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function MessageContextMenu({ position }) {
   const { xPos, yPos } = position;
-  return <div style={{ top: { yPos }, left: { xPos } }}>Hello</div>;
+  return <div style={{ top: { xPos }, left: { yPos } }}>Hello</div>;
 }
--- client/src/Comps/Messages.jsx
+++ client/src/Comps/Messages.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useCallback } from "react";
 import { Navbar, Container, Row, Col, Spinner } from "react-bootstrap";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { useConversations } from "../utils/ConvorsationProvider";
@@ -10,39 +10,69 @@ export default function Message({ sendMessage }) {
   const [messages, setMessages] = messageState;
   const [user, setUser] = userState;
   const [mobileView, setMobileView] = mobileViewState;
-  const [contextMenu, setContextMenu] = useState({});
+  const [contextMenu, setContextMenu] = useState({
+    xPos: "0px",
+    yPos: "0px",
+    show: false,
+  });
 
   const textRef = useRef();
   const { width } = useViewport();
 
-  // Setting up the ability to delete messages via a custom context menu (from "right-click")
-  useEffect(() => {
-    const messageElements = document.querySelectorAll(".message");
-    messageElements.forEach((element) => {
-      element.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-        const messageIndex = element.getAttribute("data-key");
-        const xPos = e.pageX + "px";
-        const yPos = e.pageY + "px";
+  // //! I need to research useCallback.
+  // //* I REALLY NEED TO JUST TRASH THIS WHOLE IDEA AND START FROM SCRATCH ON THE CONTEXT MENU IDEA
+  // const handleRightClick = useCallback(
+  //   (event, element) => {
+  //     event.preventDefault();
+  //     // const messageIndex = element.getAttribute("data-key");
+  //     const xPos = `${event.pageX}px`;
+  //     const yPos = `${event.pageY}px`;
+  //     console.log(xPos, yPos);
 
-        //! this does not work. I need a different way to handle the display state of the message context menu.
-        //! Possibly with custom CSS or there may be another eventListener I need
-        setContextMenu({
-          xPos,
-          yPos,
-          display: "block",
-        });
-        console.log(contextMenu);
-      });
-    });
-  }, [contextMenu]);
+  //     //! this does not work. I need a different way to handle the display state of the message context menu.
+  //     //! Possibly with custom CSS or there may be another eventListener I need
+
+  //     //! The problem is that I am setting state in an event listener,
+  //     //! this causes the page to re render and so the event listener function gets lost.
+  //     setContextMenu({
+  //       xPos: xPos,
+  //       yPos: yPos,
+  //       show: true,
+  //     });
+  //     console.log(contextMenu);
+  //   },
+  //   [contextMenu]
+  // );
+
+  // //* Setting up the ability to delete messages via a custom context menu (from "right-click")
+  // useEffect(() => {
+  //   //? May want to set up a filter for which element you click on dismiss the menu,
+  //   //? Cause obviously if you click on a setting in the context menu, you don't want the menu to disappear
+  //   document.addEventListener("click", (event) => {
+  //     setContextMenu({
+  //       ...contextMenu,
+  //       show: false,
+  //     });
+  //   });
+  //   const messageElements = document.querySelectorAll(".message");
+  //   messageElements.forEach((element) => {
+  //     element.addEventListener("contextmenu", (event) => {
+  //       handleRightClick(event, element);
+  //     });
+  //   });
+  //   //* Need to remove the event listeners before re-rendering the page.
+  //   //* Without doing this there will be errors
+  //   return () => {
+  //     messageElements.forEach((element) => {
+  //       element.removeEventListener("contextmenu", handleRightClick);
+  //     });
+  //   };
+  // }, [contextMenu, handleRightClick]);
 
   return (
     <>
-      <MessageContextMenu
-        style={{ display: contextMenu.display }}
-        position={contextMenu}
-      />
+      {contextMenu.show ? <MessageContextMenu position={contextMenu} /> : null}
+
       <div className={mobileView.messages ? "show" : "hide"}>
         {width <= 575 ? (
           <Navbar>
@@ -60,33 +90,36 @@ export default function Message({ sendMessage }) {
             </button>
           </Navbar>
         ) : null}
-        <Container id="messages">
-          {messages.length < 0 ? (
-            <Spinner id="spinner" animation="border" />
-          ) : (
-            messages.map((message, i) => {
-              return (
-                <div
-                  key={i}
-                  className={`my-1 d-flex flex-column ${
-                    message.sender_id === user._id
-                      ? "align-self-end align-items-end"
-                      : "align-items-start"
-                  }`}
-                >
-                  <div
-                    data-key={i}
-                    className={`message rounded px-2 py-1 ${
-                      message.sender_id === user._id
-                        ? "bg-primary text-white"
-                        : "border"
-                    }`}
-                  >
-                    {message.content}
-                  </div>
-                  {
-                    // for some reason, this thing breaks when you tryi to select a new conversation? No idea why
-                    /* <div
+
+        <div className="d-flex flex-column flex-grow-1" id="messages">
+          <div className="flex-grow-1 overflow-auto">
+            <div className="d-flex flex-column align-items-start justify-content-end px-3">
+              {messages.length < 0 ? (
+                <Spinner id="spinner" animation="border" />
+              ) : (
+                messages.map((message, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className={`my-1 d-flex flex-column ${
+                        message.sender_id === user._id
+                          ? "align-self-end align-items-end"
+                          : "align-items-start"
+                      }`}
+                    >
+                      <div
+                        data-key={i}
+                        className={`message rounded px-2 py-1 ${
+                          message.sender_id === user._id
+                            ? "bg-primary text-white"
+                            : "border"
+                        }`}
+                      >
+                        {message.content}
+                      </div>
+                      {
+                        //? for some reason, this thing breaks when you tryi to select a new conversation? No idea why
+                        /* <div
                     className={`text-muted small ${
                       message.sender_id === user._id ? "text-right" : ""
                     }`}
@@ -96,12 +129,14 @@ export default function Message({ sendMessage }) {
                       ? "You"
                       : message.senderName}
                   </div> */
-                  }
-                </div>
-              );
-            })
-          )}
-        </Container>
+                      }
+                    </div>
+                  );
+                })
+              )}
+            </div>
+          </div>
+        </div>
         <Container fluid id="chatBox">
           <Row>
             <Col xs={7}>
@@ -113,6 +148,7 @@ export default function Message({ sendMessage }) {
                 onClick={(e) => {
                   e.preventDefault();
                   sendMessage(textRef.current.value);
+                  textRef.current.value = "";
                 }}
               >
                 <FaArrowRight className="bg-primary" />
