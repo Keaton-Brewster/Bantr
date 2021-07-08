14143159c7c8e1af3a7185379e253fb1d643d652
Keaton-Brewster <keaton.brewster@gmail.com>
2021-07-07 21:38:41 -0500

    got emojis to load yay

--- client/src/Comps/MainContent/Messaging/ChatInput.jsx
+++ client/src/Comps/MainContent/Messaging/ChatInput.jsx
@@ -1,15 +1,44 @@
 import { useRef, useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { FaArrowRight } from "react-icons/fa";
+import { FaRegSmile } from "react-icons/fa";
 import { useConversations } from "../../../utils/ConversationProvider";
 import { useViewport } from "../../../utils/ViewportProvider";
+import Picker from "emoji-picker-react";
 import "./chatInput.css";
 
 export default function ChatInput({ containerRef }) {
+  const [currentInput, setCurrentInput] = useState();
   const { sendMessage } = useConversations();
   const { width, scrollToBottomMessages } = useViewport();
   const [chatboxWidth, setChatboxWidth] = useState("100%");
+  const [emojiPickerShow, setEmojiPickerShow] = useState(false);
   const textRef = useRef();
+
+  function messageSubmit(event) {
+    event.preventDefault();
+    if (!textRef.current.value) return;
+    scrollToBottomMessages();
+    sendMessage(currentInput);
+    textRef.current.value = "";
+  }
+
+  function handleInputChange(event) {
+    event.preventDefault();
+    setCurrentInput(textRef.current.value);
+  }
+
+  function toggleEmojiMenu(event) {
+    event.preventDefault();
+    setEmojiPickerShow(!emojiPickerShow);
+  }
+
+  function addEmoji(event, emojiData) {
+    event.preventDefault();
+    const messagePlusEmoji = `${textRef.current.value} ${emojiData.emoji}`;
+    setCurrentInput(messagePlusEmoji);
+    textRef.current.value = messagePlusEmoji;
+  }
 
   useEffect(() => {
     if (width >= 680) setChatboxWidth(`${containerRef.current.offsetWidth}px`);
@@ -22,20 +51,29 @@ export default function ChatInput({ containerRef }) {
         Obviously this would only work for computers, but it would be a good basic function to have */
     <div id="chatBox" style={{ width: chatboxWidth }}>
       <Row>
-        <Col xs={10}>
-          <textarea ref={textRef} rows="1" id="chatInput" type="text" />
+        <div className={`${emojiPickerShow ? "show" : "hide"}`}>
+          <Picker
+            disableSkinTonePicker={true}
+            onEmojiClick={addEmoji}
+            pickerStyle={{ width: "100%" }}
+          />
+        </div>
+      </Row>
+      <Row>
+        <Col xs={9}>
+          <textarea
+            ref={textRef}
+            rows="1"
+            id="chatInput"
+            type="text"
+            onChange={handleInputChange}
+          />
         </Col>
-        <Col xs={2} className="text-center">
-          <button
-            id="sendButton"
-            onClick={(e) => {
-              e.preventDefault();
-              if (!textRef.current.value) return;
-              scrollToBottomMessages();
-              sendMessage(textRef.current.value);
-              textRef.current.value = "";
-            }}
-          >
+        <Col xs={2} className="text-center chatInputButton">
+          <FaRegSmile id="openEmojisButton" onClick={toggleEmojiMenu} />
+        </Col>
+        <Col xs={1} className="text-center chatInputButton">
+          <button id="sendButton" onClick={messageSubmit}>
             <FaArrowRight className="sendButton" />
           </button>
         </Col>
