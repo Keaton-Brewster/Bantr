037abcbd932fe78250ab2d8ddc0510386ad332ec
Keaton-Brewster <keaton.brewster@gmail.com>
2021-07-08 02:55:00 -0500

    re ordered files

---
+++ server/models/Conversation.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ConversationSchema = new Schema({
+  name: String,
+
+  members: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  messages: [
+    {
+      // to: { type: Schema.Types.ObjectId, ref: "Conversation" },
+      // sender_id: { type: String, ref: "User" },
+      sender_id: { type: Schema.Types.ObjectId, ref: "User" },
+      content: String,
+      sent_at: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: Date,
+});
+
+const Conversation = mongoose.model("Conversation", ConversationSchema);
+
+module.exports = Conversation;
