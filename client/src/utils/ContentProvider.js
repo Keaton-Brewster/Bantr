e4bdee6316b857c611778ed67026fec492589f42
Keaton-Brewster <keaton.brewster@gmail.com>
2021-10-26 21:46:12 -0500

    added local storage set up for content state, but am leaving disabled to flush out other functionality

--- client/src/utils/ContentProvider.js
+++ client/src/utils/ContentProvider.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {
   useEffect,
   useState,
@@ -8,6 +9,7 @@ import {
 import { isMobile } from "react-device-detect";
 import { useViewport } from "./ViewportProvider";
 import reducer, { initialState } from "./Reducer";
+import useLocalStorage from "./useLocalStorage";
 
 const mainContentContext = createContext();
 
@@ -16,7 +18,11 @@ export function useContentContext() {
 }
 
 export default function ContentProvider({ children }) {
-  //
+  const [contentState, setContentState] = useLocalStorage(
+    "content_state",
+    "default"
+  );
+
   //* I know what I need to do to the state!
   // The thing to do is to set up a reducer that imports all the
   // components and renders them conditionally in their respective
@@ -25,17 +31,23 @@ export default function ContentProvider({ children }) {
   //! I just had what I think might be the best way to handle contect state management.
   //! USE LOCAL STORAGE YOU DUMMY. You need to be able to keep track of what is going on for the user, what converstaion theyre on,
   //! what page theyre on, etc, and what better way to get around this than by using local storage.
+
   const { width, height } = useViewport();
   const [display, setDisplay] = useState(() => {
     if (isMobile) return { menu: true, mainContent: false };
     return { menu: true, mainContent: true };
   });
-  const [activeContent, setActiveContent] = useState({
-    conversations: true,
-  });
-  const [activeMenu, setActiveMenu] = useState({
-    conversations: true,
-  });
+
+  const [activeContent, setActiveContent] = useState(
+    contentState === "default"
+      ? { conversations: true }
+      : contentState.storedActiveContent
+  );
+  const [activeMenu, setActiveMenu] = useState(
+    contentState === "default"
+      ? { conversations: true }
+      : contentState.storedActiveMenu
+  );
 
   // This handles the changes between mobile layout and desktop layout
   useEffect(() => {
@@ -53,7 +65,6 @@ export default function ContentProvider({ children }) {
         mainContent: false,
       });
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [width, height]);
 
   const value = {
@@ -64,7 +75,15 @@ export default function ContentProvider({ children }) {
     display,
     setDisplay,
   };
-  // const value = useReducer(reducer, initialState);
+
+  // useEffect(() => {
+  //   const storableContentState = JSON.stringify({
+  //     storedActiveContent: activeContent,
+  //     storedActiveMenu: activeMenu,
+  //   });
+  //   setContentState(storableContentState);
+  // }, [activeContent, activeMenu]);
+
   return (
     <mainContentContext.Provider value={value}>
       {children}
