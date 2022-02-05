4ca61c3a780350c43cc6a2a3b7661f616c69278f
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-04 21:50:05 -0600

    Merging back to main

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -5,8 +5,9 @@ const conversations = db.Conversation;
 router.get((request, response) => {
     const user_id = request.params;
     try {
+
         // I am just now redoing this controller, and ran out of time to test. Will get back to this.
-        conversations.findById(user_id).then(user => console.log(user))
+        conversations.findById(user_id => response.send(user_id));
 
     }
     catch {
