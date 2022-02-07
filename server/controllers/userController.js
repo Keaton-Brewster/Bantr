47d862318f72c98d4961d612a0bd1d1d096fe36f
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-06 19:20:06 -0600

    restructure of folders

---
+++ server/controllers/userController.js
@@ -0,0 +1,20 @@
+const router = require('express').Router()
+const db = require('../models')
+
+router.get("/login", (request, response) => {
+    try {
+        const { authorization } = request.headers
+        const buff = new Buffer(authorization, 'base64')
+        const auth = buff.toString('ascii')
+
+        db.User.findOne({ g_id: auth })
+            .then(user => { response.send(user).status(200) })
+            .catch(e => { response.sendStatus(404) })
+    }
+    catch (e) {
+        console.log(`Error in userController --- METHOD: GET --- TYPE: LOGIN --- ERROR: ${e}`)
+        response.sendStatus(500)
+    }
+})
+
+module.exports = router
