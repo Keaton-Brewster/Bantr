36f822a17c636011322da2b9841e95104b2440ad
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-09 00:01:06 -0600

    useController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,10 @@
-const router = require('express').Router()
-const passport = require('../Passport');
-const db = require('../models')
+const router = require("express").Router();
+const passport = require("../Passport");
+const db = require("../models");
 
-router.post("/login", passport.authenticate('local'), (request, response) => {
-    const { user } = request;
-    response.send(user).status(200);
-})
+router.post("/login", passport.authenticate("local"), (request, response) => {
+  const { user } = request;
+  response.send(user).status(200);
+});
 
-module.exports = router
+module.exports = router;
