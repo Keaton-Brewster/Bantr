3b788e8f6c61a4db9374f46b6cd3b4d17334c461
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-08 23:59:42 -0600

    messing around trying to cache some files

--- client/public/serviceWorker.js
+++ client/public/serviceWorker.js
@@ -1,23 +1,44 @@
-const FILES_TO_CACHE = ["~/src"];
-const CACHE_NAME = "banter_static_v1";
+const FILES_TO_CACHE = ["./src"],
+  CACHE_NAME = "banter_static_v1",
+  CACHE_VERSION = "0.1.0",
+  CURRENT_CACHES = {
+    font: CACHE_NAME + CACHE_VERSION,
+  };
+
+const cacheFiles = async () => {
+  const cache = await caches.open(CACHE_NAME);
+  return cache.addAll(FILES_TO_CACHE);
+};
+
+const removeOldCaches = async () => {
+  const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
+  caches.keys().then((cacheNames) => {
+    return Promise.all(
+      cacheNames.forEach((cacheName) => {
+        if (!expectedCacheNamesSet.has(cacheName)) {
+          // If this cache name isn't present in the set of "expected" cache names, then delete it.
+          console.log("Deleting out of date cache:", cacheName);
+          return caches.delete(cacheName);
+        }
+      })
+    );
+  });
+};
 
 /* eslint-disable no-restricted-globals */
 self.addEventListener("install", function (event) {
   console.log("Install!");
-  //! Need to, at somepoint figure out caching source files
-  //! so That app load time is quicker once youve installed it
-  // event.waitUntil(
-  //   caches.open(CACHE_NAME).then((cache) => {
-  //     console.log(FILES_TO_CACHE);
-  //     return cache.addAll(FILES_TO_CACHE);
-  //   })
-  // );
-  // console.log("Your files were pre-cached successfully!");
-  // self.skipWaiting();
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Activate!");
+  try {
+    removeOldCaches();
+    cacheFiles();
+    console.log("Activate!");
+  } catch (e) {
+    console.error(e);
+  }
 });
 self.addEventListener("fetch", function (event) {
   // console.log("Fetch!", event.request);
