28323310c76a03ced41ca6ac52517b37b557e6a5
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-14 21:52:32 -0600

    had emails and phone numbers in the wrong places

--- server/models/seeder.js
+++ server/models/seeder.js
@@ -52,29 +52,29 @@ const userSeed = [
     ObjectId("60a532c9266a4f2cc69925f6"),
     "Timmeree",
     "Estes",
-    "+19136369994",
-    "timmeree@mail.com"
+    "timmeree@mail.com",
+    "+19136369994"
   ),
   new User(
     ObjectId("60a5336e266a4f2cc69925f8"),
     "Brandy",
     "Miles",
-    "+19560002938",
-    "brandy@mail.com"
+    "brandy@mail.com",
+    "+19560002938"
   ),
   new User(
     ObjectId("60a533d0266a4f2cc69925fa"),
     "Brendon",
     "Cupertino",
-    "+17638724466",
-    "brendon@mail.com"
+    "brendon@mail.com",
+    "+17638724466"
   ),
   new User(
     ObjectId("60a5344d266a4f2cc69925fc"),
     "Maggie",
     "Ichikko",
-    "+17859698002",
-    "maggie@mail.com"
+    "maggie@mail.com",
+    "+17859698002"
   ),
 ];
 
