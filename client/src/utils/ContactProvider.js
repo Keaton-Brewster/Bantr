894e4a4ba64337d509439e8e92b9fbf9652c52b5
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-17 18:19:07 -0600

    got the call to collect contact information and update the state in working order

--- client/src/utils/ContactProvider.js
+++ client/src/utils/ContactProvider.js
@@ -22,37 +22,25 @@ export default function ContactProvider({ user, children }) {
   const [sortedContacts, setSortedContacts] = useState();
   const { contacts } = user;
 
-  //! This function does not work the way I was hoping that it would.
-  //! which is fine I will just need to sink some extra time into it.
-  const getContactInformation = async (cb) => {
-    const allContacts = await contacts.forEach((_id) => {
-      API.getContact(
-        _id,
-        (contact) => {
-          console.log(contact);
-          return contact;
-        },
-        (err) => {
-          console.error(err);
-        }
-      );
-    });
-    cb(allContacts);
+  const getContactInformation = (cb) => {
+    const id_array = contacts;
+    API.getContacts(
+      id_array,
+      (contacts) => {
+        cb(contacts);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   };
 
-
-  //! For some reason the function is not activiating within the mount
-  //! even though the mount itself is taking place and the console statement 
-  //! is coming through
   useEffect(() => {
-    console.log("contact provider mounted");
-    getContactInformation((contacts) => {
-      setSortedContacts(contacts);
+    getContactInformation((allContacts) => {
+      setSortedContacts(allContacts);
     });
 
-    return () => {
-      console.log("contact provider unmounted");
-    };
+    return () => {};
   }, []);
 
   const value = {
