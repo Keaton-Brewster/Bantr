7e839f9532d278998ed76645b03edda9f9a62836
Keaton-Brewster <keaton.brewster@gmail.com>
2022-02-17 18:18:05 -0600

    importing the contacts from the provider and using it to see live the contacts that are on your profile

--- client/src/Comps/Menu/Contacts.jsx
+++ client/src/Comps/Menu/Contacts.jsx
@@ -1,19 +1,23 @@
-import { useState } from "react";
-import { ListGroup } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { ListGroup, Spinner } from "react-bootstrap";
 import { AiFillPlusCircle } from "react-icons/ai";
 import { useConversations } from "../../utils/ConversationProvider";
 import API from "../../utils/API";
 import NewContactModal from "../Modals/NewContactModal";
+import { useContactContext } from "../../utils/ContactProvider";
 
 export default function Contacts() {
   const [newContactModal, setNewContactModal] = useState(false);
-  const [contacts, setContacts] = useState();
+  const { contacts } = useContactContext();
   const { user } = useConversations();
+  const [loading, setLoading] = useState(true);
 
   function showNewContactModal(event) {
     event.preventDefault();
     setNewContactModal(true);
   }
+
+  const selectContact = () => {};
 
   function addContact(phoneNum) {
     API.addContact(
@@ -28,7 +32,14 @@ export default function Contacts() {
     );
   }
 
-  return (
+  useEffect(() => {
+    if (!contacts) return;
+    setLoading(false);
+  }, [contacts]);
+
+  return loading ? (
+    <Spinner className="absoluteCenter" animation="border" />
+  ) : (
     <>
       <NewContactModal
         show={newContactModal}
@@ -41,15 +52,23 @@ export default function Contacts() {
           <AiFillPlusCircle id="addButton" />
           Add Contact
         </ListGroup.Item>
-        {contacts
-          ? contacts.map((contact) => {
-              return (
-                <ListGroup.Item>
-                  <p>hello</p>
-                </ListGroup.Item>
-              );
-            })
-          : null}
+        {contacts ? (
+          contacts.map((contact, index) => {
+            return (
+              <ListGroup.Item
+                className="LGItem"
+                onClick={selectContact(contact)}
+                key={index}
+              >
+                {contact.givenName + " " + contact.familyName}
+              </ListGroup.Item>
+            );
+          })
+        ) : (
+          <div>
+            <p>No Contacts here!</p>
+          </div>
+        )}
       </ListGroup>
     </>
   );
