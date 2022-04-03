7f3912b4bd1204ab4825dda25417c01662612300
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-03 17:20:18 -0500

    Added in the whole list of your contacts, with the ability to filter by searching names, into the new conversation modal so that starting new conversations with other users is more intuitive than having to input each individual phone number

--- client/src/Comps/Modals/NewConversationModal.jsx
+++ client/src/Comps/Modals/NewConversationModal.jsx
@@ -1,15 +1,45 @@
-import { useState } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { useEffect, useState, useRef } from "react";
+import { Modal, Button, ListGroup } from "react-bootstrap";
+import { useContactContext } from "../../utils/ContactProvider";
 
 export default function NewConversationModal({
+  //PROPS
+  //================================================================================
   show,
   hide,
   createConversation,
 }) {
+  //STATE
+  //================================================================================
   const [recipient, setRecipients] = useState([]);
+  const { contacts, setSelectedContact, setSearchValue } = useContactContext();
+  const [searchedContacts, setSearchedContacts] = useState(contacts);
+  const [loading, setLoading] = useState(true);
+  const searchRef = useRef();
 
+  //FUNCTIONS
+  //================================================================================
   function createRecipientCard() {}
 
+  function selectContact(event) {
+    event.preventDefault();
+    console.log("selectContact() :: ");
+  }
+
+  function handleInputChange(event) {
+    event.preventDefault();
+    setSearchValue(searchRef.current.textContent);
+  }
+
+  //EFFECTS
+  //================================================================================
+  useEffect(() => {
+    if (!contacts) return;
+    setLoading(false);
+  }, [contacts]);
+
+  //COMPONENT
+  //================================================================================
   return (
     <Modal
       show={show}
@@ -24,13 +54,36 @@ export default function NewConversationModal({
           <h4>Start a new conversation</h4>
         </Modal.Title>
       </Modal.Header>
+      <Modal.Header>
+        <small>Recipients:</small>
+      </Modal.Header>
       <Modal.Body>
-        <small>recipients</small>
-        <div></div>
-        <form>
-          <label htmlFor="number"></label>
-          <input id="" placeholder="(000) 000-0000" />
-        </form>
+        <ListGroup.Item
+          contentEditable
+          ref={searchRef}
+          className="textarea"
+          id="searchBox"
+          onInput={handleInputChange}
+        />
+        <ListGroup>
+          {contacts ? (
+            contacts.map((contact, index) => {
+              return (
+                <ListGroup.Item
+                  className="LGItem"
+                  onClick={selectContact}
+                  key={index}
+                >
+                  {contact.givenName + " " + contact.familyName}
+                </ListGroup.Item>
+              );
+            })
+          ) : (
+            <div>
+              <p>No Contacts here!</p>
+            </div>
+          )}
+        </ListGroup>
       </Modal.Body>
       <Modal.Footer>
         <Button
