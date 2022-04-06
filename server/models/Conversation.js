c507d8c39dc775a3e91566563fa9a387281e1f4e
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-06 18:36:16 -0500

    formatting

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -4,14 +4,16 @@ const { Schema } = mongoose;
 const ConversationSchema = new Schema({
   name: String,
   members: [Schema.Types.ObjectId],
-  messages: [{
-    sender_id: String,
-    content: String,
-    sent_at: {
-      type: Date,
-      default: Date.now
-    }
-  }],
+  messages: [
+    {
+      sender_id: String,
+      content: String,
+      sent_at: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
   created_at: {
     type: Date,
     default: Date.now,
