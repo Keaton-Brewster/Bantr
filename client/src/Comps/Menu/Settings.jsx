7edf4d1026547278e1f1001be32e831aa23db201
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-09 15:59:01 -0500

    added darkmode toggle button

--- client/src/Comps/Menu/Settings.jsx
+++ client/src/Comps/Menu/Settings.jsx
@@ -1,20 +1,43 @@
-import { Button, ListGroup } from "react-bootstrap";
+import React from "react";
+import { Button, ListGroup, Form } from "react-bootstrap";
+import DarkModeToggle from "react-dark-mode-toggle";
 import useLocalStorage from "../../utils/useLocalStorage";
+import { useThemes } from "../../utils/ThemeProvider";
 
 export default function Settings() {
+  //STATE
+  //================================================================================
   const [user, setUser] = useLocalStorage("user", 0);
+  const { theme, setTheme } = useThemes();
 
+  //FUNCTIONS
+  //================================================================================
   function logout(event) {
     event.preventDefault();
     setUser(0);
     window.location.href = "/";
   }
 
+  function handleThemeToggle(isDarkMode) {
+    if (isDarkMode) setTheme("dark");
+    else if (!isDarkMode) setTheme("light");
+  }
+
+  //COMPONENT
+  //================================================================================
   return (
     <>
       <ListGroup>
-        <ListGroup.Item className="text-center">
+        <ListGroup.Item className="LGItem text-center">
           <Button onClick={logout}>Logout</Button>
+        </ListGroup.Item>
+        <ListGroup.Item className="LGItem">
+          <span>App Theme </span>
+          <DarkModeToggle
+            onChange={handleThemeToggle}
+            checked={theme === "dark" ? true : false}
+            size={80}
+          />
         </ListGroup.Item>
       </ListGroup>
     </>
