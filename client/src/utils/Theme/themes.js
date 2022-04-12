5c717e3ec896ed29265e5f261933fa781ab43ee2
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-11 19:30:25 -0500

    Trying to get theme provider to work for all components, and not just the global styles and single LGItem, so that I can change the color of the top menu with the change from light, to dark mode. No luck yet but will continue working.

--- client/src/utils/Theme/themes.js
+++ client/src/utils/Theme/themes.js
@@ -1,11 +1,15 @@
 export const lightTheme = {
+  name: "light",
   body: "#fefffb",
   text: "#232425",
+  headers: "#101010",
   gradient: "linear-gradient(#39598A, #79D7ED)",
 };
 
 export const darkTheme = {
+  name: "dark",
   body: "#232425",
   text: "#fefffb",
+  headers: "#101010",
   gradient: "linear-gradient(#091236, #1E215D)",
 };
