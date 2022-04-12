2140ede9a4c700d27631b3cb06f533f080fa8acf
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-11 19:30:21 -0500

    Trying to get theme provider to work for all components, and not just the global styles and single LGItem, so that I can change the color of the top menu with the change from light, to dark mode. No luck yet but will continue working.

--- client/src/utils/ThemeProvider.js
+++ client/src/utils/ThemeProvider.js
@@ -1,4 +1,4 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import useLocalStorage from "./useLocalStorage";
 import { GlobalStyles } from "./Theme/global";
 import { lightTheme, darkTheme } from "./Theme/themes";
@@ -13,16 +13,29 @@ export default function ThemeProvider({ children }) {
   //STATE
   //================================================================================
   const [theme, setTheme] = useLocalStorage("theme", "light");
+  const [themeName, setThemeName] = useState(theme);
 
   //VALUE
   //================================================================================
-  const value = { theme, setTheme };
+  const value = { theme, setThemeName };
+
+  //EFFECTS
+  //================================================================================
+  useEffect(() => {
+    if (themeName === "light") setTheme(lightTheme);
+    else if (themeName === "dark") setTheme(darkTheme);
+
+    return () => {
+      setThemeName(null);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [themeName]);
 
   //COMPONENT
   //================================================================================
   return (
     <themeContext.Provider value={value}>
-      <GlobalStyles theme={theme === "light" ? lightTheme : darkTheme} />
+      <GlobalStyles theme={theme} />
       {children}
     </themeContext.Provider>
   );
