5fa0b39da89b5171f4b508bf33687a538955024b
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-11 21:43:01 -0500

    adding more props to keep functionality. But shortly I need to create a new component to replace the editable LGItem used in the new conversation modal

--- client/src/Comps/Menu/LGItems/index.jsx
+++ client/src/Comps/Menu/LGItems/index.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { ListGroup } from "react-bootstrap";
 import styled from "styled-components";
 
-function _LGItem({ className, children, onClick }) {
-  return (
-    <ListGroup.Item className={className} onClick={onClick}>
-      {children}
-    </ListGroup.Item>
-  );
-}
+const _LGItem = forwardRef(
+  ({ className, children, onClick, contentEditable, onInput }, ref) => {
+    return (
+      <ListGroup.Item
+        contentEditable={contentEditable}
+        ref={ref}
+        className={className}
+        onClick={onClick}
+        onInput={onInput}
+      >
+        {children}
+      </ListGroup.Item>
+    );
+  }
+);
 
 const LGItem = styled(_LGItem)`
   transition: ${({ BGTransition }) =>
     BGTransition ? "background 0.5s ease" : "background 0s linear !important"};
+  background-color: ${({ theme }) => theme.body};
 `;
 
 export default LGItem;
