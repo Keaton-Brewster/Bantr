9126ad197c680f89c01a3bb8158d744674302ea1
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-14 11:32:55 -0500

    turned SingleMessage.jsx into styled component so that it can be styled on its own using sytled-components

--- client/src/Comps/MainContent/Messaging/SingleMessage.jsx
+++ client/src/Comps/MainContent/Messaging/SingleMessage.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
+import styled from "styled-components";
 
-export default function SingleMessage({
+function SingleMessage({
   //PROPS
   //================================================================================
   data,
   handleRightClick,
   index,
+  className,
 }) {
   //STATE
   //================================================================================
@@ -29,20 +31,18 @@ export default function SingleMessage({
   return (
     <div
       id={`message_${index}`}
-      className={`my-1 d-flex flex-column ${
+      className={`my-1 d-flex flex-column ${className} ${
         message.fromMe ? "align-self-end align-items-end" : "align-items-start"
       }`}
     >
-
       <div
-        // data-key={i}
         className={`message round-border px-2 py-1 ${
           message.fromMe ? "messageFromMe" : "messageToMe"
         }`}
       >
         {message.content}
       </div>
-      
+
       {
         <div
           className={`text-muted small ${message.fromMe ? "text-right" : ""}`}
@@ -57,3 +57,13 @@ export default function SingleMessage({
     </div>
   );
 }
+
+export default styled(SingleMessage)`
+  > div.message {
+    overflow-wrap: break-word;
+    white-space: break-spaces;
+    transition: background 0s linear;
+    background-color: ${({ data }) => (data[0].fromMe ? "#6ab71c" : "#e9e9e9")};
+    color: ${({ data }) => (data[0].fromMe ? "#fefffb" : "#232425")};
+  }
+`;
