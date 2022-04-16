ff94ccbc8f29e4624198881d580fba3419e6e8b4
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-15 22:26:41 -0500

    passing theme down into main comps, so that I can utilize it where I need to.

--- client/src/Comps/Menu/index.jsx
+++ client/src/Comps/Menu/index.jsx
@@ -1,18 +1,21 @@
 import { useRef } from "react";
+import { useUIContext } from "../../utils/UIProvider";
+import { useThemes } from "../../utils/ThemeProvider";
+
 import MenuBar from "./MenuBar";
 import Conversations from "./Conversations";
 import Contacts from "./Contacts";
 import Settings from "./Settings";
-import { useUIContext } from "../../utils/UIProvider";
 
 export default function Sidebar() {
+  const { theme } = useThemes();
   const { activeMenu, display } = useUIContext();
   const containerRef = useRef();
 
   function renderSwitch() {
-    if (activeMenu.conversations) return <Conversations />;
-    if (activeMenu.contacts) return <Contacts />;
-    if (activeMenu.settings) return <Settings />;
+    if (activeMenu.conversations) return <Conversations theme={theme} />;
+    if (activeMenu.contacts) return <Contacts theme={theme} />;
+    if (activeMenu.settings) return <Settings theme={theme} />;
   }
 
   return (
@@ -20,7 +23,7 @@ export default function Sidebar() {
       {/* <div>hello</div> */}
       <div className={display.menu ? "show" : "hide"}>
         {renderSwitch()}
-        <MenuBar containerRef={containerRef} />
+        <MenuBar containerRef={containerRef} theme={theme} />
       </div>
     </div>
   );
