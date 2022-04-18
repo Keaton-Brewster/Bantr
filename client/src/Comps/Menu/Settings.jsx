f941d8e5e53ab00a98fc98f07e5f3edb2b28da3c
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-18 12:35:16 -0500

    Had to rework the theme provider (hopefully one last time) Didn't think about that calling a custom hook in multiple different places creates different instances of that state, which will not be shared across the whole app. So I had to rebuild the custom provider from before, but make a few minor tweaks to make sure it works properly.

--- client/src/Comps/Menu/Settings.jsx
+++ client/src/Comps/Menu/Settings.jsx
@@ -1,16 +1,18 @@
 import React, { useEffect } from "react";
 import useLocalStorage from "../../utils/useLocalStorage";
-import useTheme from "../../utils/useTheme";
+import { useThemes } from "../../utils/ThemeProvider";
 
 import { Button, ListGroup } from "react-bootstrap";
 import LGItem from "./LGItem";
 import DarkModeToggle from "react-dark-mode-toggle";
 
+import { lightTheme, darkTheme } from "../../utils/Theme/themes";
+
 export default function Settings() {
   //STATE
   //================================================================================
   const [user, setUser] = useLocalStorage("user", 0);
-  const [theme, setTheme] = useTheme();
+  const [theme, setTheme] = useThemes();
 
   //FUNCTIONS
   //================================================================================
@@ -21,8 +23,8 @@ export default function Settings() {
   }
 
   function handleThemeToggle(isLightMode) {
-    if (isLightMode) setTheme("dark");
-    else if (!isLightMode) setTheme("light");
+    if (isLightMode) setTheme(darkTheme);
+    else if (!isLightMode) setTheme(lightTheme);
   }
 
   //EFFECTS
