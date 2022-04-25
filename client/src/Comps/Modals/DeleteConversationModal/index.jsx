fbb16a0ca795f07e9388c48dea1f395689745eda
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-25 12:54:23 -0500

    commenting on next steps. I am likely going to need to spend some time reworking the way I am storing conversations. Poor planning on my behalf

--- client/src/Comps/Modals/DeleteConversationModal/index.jsx
+++ client/src/Comps/Modals/DeleteConversationModal/index.jsx
@@ -1,15 +1,35 @@
 import React, { useState, useRef, useEffect } from "react";
-import styled from "styled-components";
+import styled, { withTheme } from "styled-components";
+
+import { useUserContext } from "../../../utils/UserProvider";
+import API from "../../../utils/API";
 
 import { Modal, Button } from "react-bootstrap";
+import { useConversations } from "../../../utils/ConversationProvider";
 
-function DeleteConversationModal({ show, hide, className }) {
+function DeleteConversationModal({
+  show,
+  hide,
+  className,
+  targetIndex,
+  theme,
+}) {
   // STATES
   //================================================================================
+  const { user } = useUserContext();
+  const { conversations } = useConversations();
   // FUNCTIONS
   //================================================================================
   function deleteConversation(e) {
     e.preventDefault();
+    console.log(targetIndex);
+    // This function will need to send the conversation._id and the user._id to the server
+    // And then the server will need to add that conversation._id to a list on the user model
+    // And use that list to filter out the conversation when returning all conversations to the client
+    //! Above method is not the way to go, I am going to need to overhaul the 
+    //! way conversations are stored in order to get this to work the way 
+    //! I want it to.
+    // API.hideConversation(conversations[targetIndex]._id, user._id);
   }
   // EFFECTS
   //================================================================================
@@ -28,14 +48,19 @@ function DeleteConversationModal({ show, hide, className }) {
         <Modal.Title id="contained-modal-title-vcenter">
           <h4>Are you sure you want to delete this conversation?</h4>
         </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Button onClick={deleteConversation}>Yes</Button>
+        <br />
+        <Button
+          style={{ backgroundColor: `${theme.danger}` }}
+          onClick={deleteConversation}
+        >
+          Yes
+        </Button>
         <Button onClick={hide}>No</Button>
-      </Modal.Body>
-      <Modal.Footer></Modal.Footer>
+      </Modal.Header>
     </Modal>
   );
 }
 
-export default styled(DeleteConversationModal)``;
+const styledComp = styled(DeleteConversationModal)``;
+
+export default withTheme(styledComp);
