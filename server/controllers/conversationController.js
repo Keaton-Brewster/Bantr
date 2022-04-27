c8c18840414684d9f0d6b5113524747048dbd95e
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-27 11:17:53 -0500

    In order to load conversations for the user, I am now storing an array of the conversation._id's that that user is apart of. this way, when a user "deletes" a conversation, it doesnt remove the whole document from the database, and thus delete the conversation for other users as well. It will instead just remove that conversations _id from the user document so that the conversation will not be loaded into the users instance. Also as a result of this, i needed a way to notify the user if they have been sent a message in a conversation that they have "deleted", so before every send message route, I am checking if the users listed in the conversation also have the conversation _id listed on their user, and pushing the conversation _id if it is now.

--- server/controllers/conversationController.js
+++ server/controllers/conversationController.js
@@ -5,18 +5,21 @@ const { ObjectId } = mongoose.Types;
 
 //todo I WILL NEED TO COME IN AT SOMEPOINT AND MAKE SURE THAT ALL CONVERSATIONS ARE BEING SENT BACK IN REVERSE CHRONILOGICAL ORDER
 
-router.get("/:id", (req, res) => {
-  const userId = req.params.id;
+router.get("/:user_id", (req, res) => {
+  const { user_id } = req.params;
+
   try {
-    db.Conversation.find({ members: { $in: [ObjectId(userId)] } })
-      .sort("-updated_at")
-      .then((convos) => {
-        res.send(convos);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.sendStatus(404);
-      });
+    db.User.findOne({ _id: user_id }).then((user) => {
+      db.Conversation.find({ _id: { $in: user.conversations } })
+        .sort("-updated_at")
+        .then((convos) => {
+          res.send(convos);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.sendStatus(404);
+        });
+    });
   } catch (error) {
     console.error(error);
     res.send(500);
@@ -63,28 +66,42 @@ router.get("/getInfo/:convo_id", async (req, res) => {
 });
 
 router.put("/newMessage", (request, response) => {
-  const { message_info, conversation_id } = request.body;
+  const { message_info, conversation } = request.body;
 
   try {
     const message = {
       content: message_info.content,
       sender_id: ObjectId(message_info.sender_id),
     };
-
-    db.Conversation.findOne({ _id: conversation_id })
-      .then((doc) => {
-        if (doc) {
-          doc.messages.push(message);
-          doc.updated_at = Date.now();
-          doc.save().then((doc) => response.send(doc).status(200));
-        } else {
-          response.sendStatus(404);
-        }
-        // response.send(result).status(200);
+    /* Because I am using a seperate array of the conversation_ids 
+    Stored on each user, Every time a message is sent, I need to check
+    if the user has 'deleted that conversation' because if so, it needs to 
+    reappear for them so that they see the new messages
+    */
+    db.User.find({ _id: { $in: conversation.members } })
+      .then((users) => {
+        users.forEach((user) => {
+          if (!user.conversations.includes(conversation._id)) {
+            user.conversations.push(conversation._id);
+            user.save();
+          }
+        });
       })
-      .catch((err) => {
-        console.error(err);
-        response.send(err).status(400);
+      .then(() => {
+        db.Conversation.findOne({ _id: conversation._id })
+          .then((convo) => {
+            if (convo) {
+              convo.messages.push(message);
+              convo.updated_at = Date.now();
+              convo.save().then((convo) => response.send(convo).status(200));
+            } else {
+              response.sendStatus(404);
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            response.send(err).status(400);
+          });
       });
   } catch (e) {
     console.error(e);
@@ -124,20 +141,38 @@ router.post("/newConversation", (req, res) => {
       },
       (err, exists) => {
         if (exists) {
-          db.Conversation.findOne({
-            members: {
-              $size: newConversation.members.length,
-              $all: newConversation.members,
-            },
-          })
-            .then((doc) => {
-              res.status(202).send(doc);
-            })
-            .catch((err) => res.send(err).status(500));
+          db.User.find({ _id: { $in: newConversation.members } }).then(
+            (users) => {
+              users.forEach((user) => {
+                if (!user.conversations.includes(newConversation._id)) {
+                  user.conversations.push(newConversation._id);
+                  user.save();
+                }
+              });
+              db.Conversation.findOne({
+                members: {
+                  $size: newConversation.members.length,
+                  $all: newConversation.members,
+                },
+              })
+                .then((doc) => {
+                  res.status(202).send(doc);
+                })
+                .catch((err) => res.send(err).status(500));
+            }
+          );
         } else if (!exists) {
           db.Conversation.create(newConversation)
             .then((response) => {
-              res.status(200).send(response);
+              db.User.find({ _id: { $in: newConversation.members } }).then(
+                (users) => {
+                  users.forEach((user) => {
+                    user.conversations.push(response._id);
+                    user.save();
+                  });
+                  res.status(200).send(response);
+                }
+              );
             })
             .catch((error) => {
               res.send(500);
@@ -157,7 +192,7 @@ router.post("/newConversation", (req, res) => {
 
 router.put("/hideConversation", (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
     db.Conversation.findOneAndUpdate({});
     res.sendStatus(200);
   } catch (err) {
