6d0215d25572a1824525b9aeca9b88ebc07f1ac3
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-27 11:19:42 -0500

    removed password, because user auth will take place solely through google. also added conversation field to user model, to store the conversations that the user wants displayed. also added function to generate random gravitar for user upon inital model creation.

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,39 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+function generatePictureSeed() {
+  let randomSet = []; // Array for a bunch of random, possible characters, that will then go on to be randomly chosen from for the final password
+  let ensureSet = []; // Array for ensuring that at least one of each selected char-set will be included in the final password
+  let seed = []; // Array for the final, randomly generated password.
+
+  const range =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+  randomSelector(range);
+  charsetEnsure(range);
+
+  function randomSelector(x) {
+    for (let i = 0; i < 100; i++) {
+      xChar = x.charAt(Math.floor(Math.random() * x.length));
+      randomSet.push(xChar);
+    }
+  }
+
+  function charsetEnsure(x) {
+    xChar = x.charAt(Math.floor(Math.random() * x.length));
+    ensureSet.push(xChar);
+  }
+
+  seed.push(ensureSet.join(""));
+
+  for (let i = 0; i < 100 - ensureSet.length; i++) {
+    var charPicked = randomSet[Math.floor(Math.random() * randomSet.length)];
+    seed.push(charPicked);
+  }
+
+  return seed.join("").toString() + ".svg";
+}
+
 const userSchema = new Schema({
   g_id: {
     type: String,
@@ -10,10 +43,6 @@ const userSchema = new Schema({
     required: true,
     unique: true,
     match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
-  },
-  password: {
-    type: String,
-    required: true,
   },
   createdAt: {
     type: Date,
@@ -30,10 +59,13 @@ const userSchema = new Schema({
   },
   imageUrl: {
     type: String,
-    default:
-      "https://www.personality-insights.com/wp-content/uploads/2017/12/default-profile-pic-e1513291410505.jpg",
+    default: generatePictureSeed(),
   },
   contacts: [],
+  conversations: {
+    type: [Schema.Types.ObjectId],
+    default: [],
+  },
 });
 
 const User = mongoose.model("User", userSchema);
