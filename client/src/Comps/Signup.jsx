9221894c28b326c2901ad6dccca6abf46d852177
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-30 10:53:00 -0500

    Had to basically completely rework the signup component/functions. With the switch to firebase, the data that I am pulling from the google api looks different, so I have to require the user provide their own first and last name (the api just sends the full name of the user that signed in.) created custom firebaseSignIn() function that lives in the utils/firebase. Had to also add state and refs to handle the name inputs that I added.

--- client/src/Comps/Signup.jsx
+++ client/src/Comps/Signup.jsx
@@ -1,19 +1,32 @@
-import { useState, useEffect } from "react";
-import GoogleLogin from "react-google-login";
+import { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+
 import { Container, Row, Form } from "react-bootstrap";
 import Header from "./Header";
 import { useViewport } from "../utils/ViewportProvider";
-import API from "../utils/API";
 import PhoneInput from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 
-export default function Signup({ setUser }) {
-  //
+import API from "../utils/API";
+import { firebaseSignIn } from "../firebase";
+
+function Signup({ setUser, className }) {
+  // STATE
+  //================================================================================
   const { width } = useViewport();
   const [formWidth, setFormWidth] = useState("100%");
-  const [phoneNum, setPhoneNum] = useState();
+  const [phoneNum, setphoneNUM] = useState();
+  const [givenName, setGivenName] = useState("");
+  const [familyName, setFamilyName] = useState("");
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
+  // REFS
+  //================================================================================
+  const givenNameRef = useRef();
+  const familyNameRef = useRef();
+
+  // FUNCTIONS
+  //================================================================================
   function handleSignup(newUser) {
     API.signup(
       newUser,
@@ -24,52 +37,81 @@ export default function Signup({ setUser }) {
       },
       (error) => {
         alert("That user already exists. Please sign in.");
-        console.error(error);
+        console.log(error);
       }
     );
   }
 
-  const googleSignup = (response) => {
-    if (response.error) return;
+  function googleSignup(event) {
+    event.preventDefault();
+    if (!givenName || !familyName || !phoneNum) return;
 
-    const { profileObj } = response;
-    const { email, familyName, givenName, imageUrl, googleId } = profileObj;
+    firebaseSignIn(
+      (user) => {
+        const { email, photoURL, uid } = user;
+        const newUser = {
+          email,
+          givenName,
+          familyName,
+          photoURL,
+          phoneNum,
+          guid: uid,
+        };
+        console.log(newUser);
+        handleSignup(newUser);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 
-    const newUser = {
-      email,
-      familyName,
-      givenName,
-      imageUrl,
-      phoneNum,
-      g_id: googleId,
-    };
+  function handleGivenNameChange(e) {
+    e.preventDefault();
+    if (!givenNameRef.current.value) return;
+    setGivenName(givenNameRef.current.value);
+  }
 
-    handleSignup(newUser);
-  };
+  function handleFamilyNameChange(e) {
+    e.preventDefault();
+    if (!familyNameRef.current.value) return;
+    setFamilyName(familyNameRef.current.value);
+  }
 
+  // EFFECTS
+  //================================================================================
   useEffect(() => {
     if (width < 680) setFormWidth("100%");
-    setFormWidth(`${width / 2}px`);
+    setFormWidth(`${width / 1.5}px`);
     return () => {
       return;
     };
   }, [width]);
 
   useEffect(() => {
-    if (phoneNum === undefined || phoneNum === null) return;
+    if (
+      phoneNum === undefined ||
+      phoneNum === null ||
+      !familyName ||
+      !givenName
+    )
+      return;
+
     let mutablePN = phoneNum.replace(/[+]/g, "");
-
-    if (mutablePN.match(/\d/g).length === 11) setButtonDisabled(false);
+    if (mutablePN.match(/\d/g).length === 11 && givenName && familyName)
+      setButtonDisabled(false);
     else setButtonDisabled(true);
-  }, [phoneNum]);
+  }, [phoneNum, familyName, givenName]);
 
+  // RENDER
+  //================================================================================
   return (
-    <>
+    <div className={className}>
       <Header />
 
       <Container style={{ width: formWidth }}>
         <Row className="justify-content-center">
-          <Form id="form" className="text-center">
+          <Form id="form" className={`text-center`}>
             <h2>Sign up here to get started</h2>
             <h5 className="mb-3 font-light">
               {/* Need to make this font not bold */}
@@ -83,17 +125,36 @@ export default function Signup({ setUser }) {
               defaultCountry="US"
               placeholder="+0 000 000 0000"
               value={phoneNum}
-              onChange={setPhoneNum}
+              onChange={setphoneNUM}
             />
             <div className="m-5">
-              <GoogleLogin
+              <input
+                onChange={handleGivenNameChange}
+                placeholder="First Name"
+                type="text"
+                ref={givenNameRef}
+              ></input>
+              <input
+                onChange={handleFamilyNameChange}
+                placeholder="Last Name"
+                type="text"
+                ref={familyNameRef}
+              ></input>
+              <button
+                className="button"
+                onClick={googleSignup}
+                disabled={buttonDisabled}
+              >
+                Sign up using your google account
+              </button>
+              {/* <GoogleLogin
                 clientId="957666672016-3850ch4mr24gvr89bmt514bn7u359mb4.apps.googleusercontent.com"
                 buttonText="Sign up"
                 onSuccess={googleSignup}
                 onFailure={googleSignup}
                 cookiePolicy={"single_host_origin"}
                 disabled={buttonDisabled}
-              />
+              /> */}
             </div>
           </Form>
         </Row>
@@ -104,6 +165,13 @@ export default function Signup({ setUser }) {
           </span>
         </Row>
       </Container>
-    </>
+    </div>
   );
 }
+
+export default styled(Signup)`
+  & .button:disabled {
+    background-color: black;
+    color: grey;
+  }
+`;
