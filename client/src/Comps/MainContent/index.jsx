8f8020825562ad1aae2fc0805edbf340ab7d1def
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-30 10:55:05 -0500

    trying to work out a contact rendering issue. for some reason, when you have no contacts and load the contact page, the main content to the right still just shows message screen, so there us something fishy going on in the state there. Have not found fix as of yet

--- client/src/Comps/MainContent/index.jsx
+++ client/src/Comps/MainContent/index.jsx
@@ -10,21 +10,19 @@ import Contacts from "./Contacts";
 import Messaging from "./Messaging";
 
 function MainContent({ className }) {
+  // STATE
+  //================================================================================
   const { selectedConversation } = useConversations();
   // Container ref is used to give refernce of width to the
   // Chat input so that is always is 100% width of its parent
   const containerRef = useRef();
   const { display, activeContent } = useUIContext();
 
-  function renderSwitch() {
-    if (!selectedConversation) return <NoContent />;
-    if (activeContent.conversations)
-      return <Messaging containerRef={containerRef} />;
-    if (activeContent.conversationInfo)
-      return <ConversationInfoScreen containerRef={containerRef} />;
-    if (activeContent.contacts) return <Contacts containerRef={containerRef} />;
-  }
+  // FUNCTIONS
+  //================================================================================
 
+  // RENDER
+  //================================================================================
   return (
     <Col
       sm={8}
@@ -32,7 +30,17 @@ function MainContent({ className }) {
       ref={containerRef}
       style={{ overflow: "hidden !important" }}
     >
-      {renderSwitch()}
+      {!selectedConversation ? (
+        <NoContent />
+      ) : activeContent.conversations ? (
+        <Messaging containerRef={containerRef} />
+      ) : activeContent.conversationInfo ? (
+        <ConversationInfoScreen containerRef={containerRef} />
+      ) : activeContent.contacts ? (
+        <Contacts containerRef={containerRef} />
+      ) : (
+        <NoContent />
+      )}
     </Col>
   );
 }
