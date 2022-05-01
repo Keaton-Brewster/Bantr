0a140e3e6da9726edb69637b4e364fbadc7a685d
Keaton-Brewster <keaton.brewster@gmail.com>
2022-04-30 19:52:08 -0500

    making it so that if the user signs out, or somehow lost in local storage, the content state in local storage gets reset

--- client/src/utils/UIProvider.js
+++ client/src/utils/UIProvider.js
@@ -3,7 +3,7 @@ import { useEffect, useState, createContext, useContext } from "react";
 import { isMobile } from "react-device-detect";
 import { useViewport } from "./ViewportProvider";
 import useLocalStorage from "./useLocalStorage";
-import { useConversations } from "./ConversationProvider";
+import { useUserContext } from "./UserProvider";
 
 const UIContext = createContext();
 
@@ -12,8 +12,10 @@ export function useUIContext() {
 }
 
 export default function UIProvider({ children }) {
-  // STATE
   //================================================================================
+  //* STATE
+  //================================================================================
+  const { user } = useUserContext();
   const { width, height } = useViewport();
   const [contentState, setContentState] = useLocalStorage(
     "content_state",
@@ -41,7 +43,8 @@ export default function UIProvider({ children }) {
       : contentState.storedActiveMenu
   );
 
-  // EFFECTS
+  //================================================================================
+  //* EFFECTS
   //================================================================================
   // This handles the changes between mobile layout and desktop layout
   useEffect(() => {
@@ -61,6 +64,7 @@ export default function UIProvider({ children }) {
     }
   }, [width, height]);
 
+  // update the local storage with the current menu state and such
   useEffect(() => {
     setContentState({
       storedActiveContent: activeContent,
@@ -68,7 +72,15 @@ export default function UIProvider({ children }) {
     });
   }, [activeContent, activeMenu]);
 
-  // VALUE FOR PROVIDER
+  // if the user signs out, or is lost from local storage for any reason, reset
+  // local storage value
+  useEffect(() => {
+    if (user === 0) return setContentState("default");
+    else return;
+  }, [user]);
+
+  //================================================================================
+  //* VALUE FOR PROVIDER
   //================================================================================
   const value = {
     activeContent,
@@ -79,5 +91,8 @@ export default function UIProvider({ children }) {
     setDisplay,
   };
 
+  //================================================================================
+  //* RENDER
+  //================================================================================
   return <UIContext.Provider value={value}>{children}</UIContext.Provider>;
 }
